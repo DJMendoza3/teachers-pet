55d3cd3447e9c15fcf349c92ed0e8535f78d79be
DJMendoza3 <DJM3webdev@gmail.com>
2023-05-06 09:01:19 -0500

    Added route structure to container and links to navbar.

--- src/layout/navbar/DesktopNav.tsx
+++ src/layout/navbar/DesktopNav.tsx
@@ -4,9 +4,9 @@ export default function DesktopNav() {
     return(
         <nav className="bg-gray-600 w-40 h-screen flex flex-col">
             <Link to={""}>Home</Link>
-            <Link to={""}>Profile</Link>
-            <Link to={""}>Tests</Link>
-            <Link to={""}>Generator</Link>
+            <Link to={"profile"}>Profile</Link>
+            <Link to={"tests"}>Tests</Link>
+            <Link to={"generator"}>Generator</Link>
             <Link to={""}>Grading</Link>
             <Link to={""}>Lesson Plans</Link>
         </nav>
--- src/main.tsx
+++ src/main.tsx
@@ -13,20 +13,47 @@ import Main from "./layout/main/Main";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Container setup={<Setup />}>
-      <Navbar />
-      <Main>
+    element: (
+      <Container setup={<Setup />}>
         <Outlet />
-      </Main>
-    </Container>,
+      </Container>
+    ),
     children: [
       {
         path: "/",
-        element: <p>Home</p>
-      }
-    ]
-  }
-
+        element: <p>Home</p>,
+      },
+      {
+        path: "/portal",
+        element: (
+          <>
+            <Navbar />
+            <Main>
+              <Outlet />
+            </Main>
+          </>
+        ),
+        children: [
+          {
+            path: "",
+            element: <p>Portal</p>,
+          },
+          {
+            path: "generator",
+            element: <p>Generator</p>,
+          },
+          {
+            path: "tests",
+            element: <p>Tests</p>,
+          },
+          {
+            path: "profile",
+            element: <p>Profile</p>,
+          }
+        ],
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
