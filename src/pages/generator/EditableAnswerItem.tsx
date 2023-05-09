2f79df8e6749cb5bf65368d81b2685adf9e2316f
DJMendoza3 <DJM3webdev@gmail.com>
2023-05-09 09:44:49 -0500

    Refactored test display so that components are in separate files.

---
+++ src/pages/generator/EditableAnswerItem.tsx
@@ -0,0 +1,47 @@
+import {useState} from "react";
+
+interface EditableAnswerItemProps {
+  index: number;
+  answer: string;
+  editAnswer: (number: number, value: string) => void;
+  deleteAnswer: (number: number) => void;
+}
+
+export default function EditableAnswerItem({
+  index,
+  answer,
+  editAnswer,
+  deleteAnswer,
+}: EditableAnswerItemProps) {
+  const [editable, setEditable] = useState(false);
+  const [value, setValue] = useState(answer);
+
+  function toggleEditable() {
+    setEditable(!editable);
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+  }
+
+  return (
+    <li key={index}>
+      {editable ? (
+        <>
+          <input type="text" defaultValue={answer} onChange={handleChange} />
+          <button
+            onClick={() => {
+              toggleEditable();
+              editAnswer(index, value);
+            }}
+          >
+            Save
+          </button>
+        </>
+      ) : (
+        <p onClick={() => toggleEditable()}>{answer}</p>
+      )}
+      <button onClick={() => deleteAnswer(index)}>Delete</button>
+    </li>
+  );
+}
