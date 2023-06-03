90b82d40bcdcd8aec22eb47c5785a8bdaa1f2af3
DJMendoza3 <DJM3webdev@gmail.com>
2023-06-03 10:04:49 -0500

    Replaced placeholder logo with actual. Updated navbar active to reflect current tab.

--- src/layout/navbar/DesktopNav.tsx
+++ src/layout/navbar/DesktopNav.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
+import apple_logo from "assets/images/apple-logo.png";
+
 export default function DesktopNav() {
   const [navOpen, setNavOpen] = useState(false);
   const location = useLocation();
@@ -12,29 +14,10 @@ export default function DesktopNav() {
   };
 
   return (
-    // <nav className={`bg-gray-600 ${navOpen ? 'w-60' : 'w-14'} h-screen flex flex-col transition-width duration-200 ease-in-out overflow-hidden`}>
-    //     <div className="flex flex-row">
-    //         <button onClick={() => toggleNav()} className="w-5 h-5"><img src={hamburger} alt="" /></button>
-    //     </div>
-
-    //     <Link to={""} className="flex"> <img src={home_icon} alt="" className="w-14"/> Home</Link>
-    //     <Link to={"profile"} className="flex"> <img src={profile_icon} alt="" className="w-14"/> Profile</Link>
-    //     <Link to={"tests"} className="flex"> <img src={main_icon} alt="" className="w-14"/> Tests</Link>
-    //     <Link to={"generator"} className="flex"> <img src={main_icon} alt="" className="w-14"/> Generator</Link>
-    //     <Link to={""} className="flex"> <img src={main_icon} alt="" className="w-14"/> Grading</Link>
-    //     <Link to={""} className="flex"> <img src={main_icon} alt="" className="w-14"/> Lesson Plans</Link>
-    // </nav>
-    <div className="flex flex-col items-center w-40 h-screen overflow-hidden text-gray-400 bg-gray-900">
+    <nav className="flex flex-col items-center w-40 h-screen overflow-hidden text-gray-400 bg-gray-900">
       <Link className="flex items-center w-full px-3 mt-3" to="#">
-        <svg
-          className="w-8 h-8 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-        </svg>
-        <span className="ml-2 text-sm font-bold">The App</span>
+        <img src={apple_logo} alt="" className="w-7"/>
+        <span className="ml-2 text-sm font-bold">Teachers Pet</span>
       </Link>
       <div className="w-full px-2">
         <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
@@ -203,6 +186,6 @@ export default function DesktopNav() {
         </svg>
         <span className="ml-2 text-sm font-medium">Logout</span>
       </Link>
-    </div>
+    </nav>
   );
 }
