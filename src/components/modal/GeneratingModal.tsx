71ee226b83e721117ca924bbcaa7891e6296a83f
DJMendoza3 <DJM3webdev@gmail.com>
2023-07-07 17:23:03 -0500

    Added a loading modal to the generator form so when a test generation request is being handled the app says loading.

---
+++ src/components/modal/GeneratingModal.tsx
@@ -0,0 +1,8 @@
+export default function GeneratingModal() {
+    return(
+        <section className="fixed w-1/3 h-1/2 bg-blue-700">
+            <p className="m-auto">Loading...</p>
+            <div className="lds-dual-ring"></div>
+        </section>
+    );
+}
